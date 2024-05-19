dcb3473c1c6337bedcae1cf2b2b48287660ce150
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-05-19 09:58:43 -0300

    environmentalizing with docker

--- ms-config-server/src/main/java/com/arthurlevi/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/arthurlevi/msconfigserver/MsConfigServerApplication.java
@@ -8,20 +8,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
-public class MsConfigServerApplication implements CommandLineRunner {
+public class MsConfigServerApplication{
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsConfigServerApplication.class, args);
 	}
 
-
-	@Override
-	public void run(String... args) throws Exception {
-		String usergithub =System.getenv("USERGITHUB");
-		System.out.println("USERNAME=" + usergithub);
-//		System.out.println("USERPASS=" + pass);
-	}
 }
